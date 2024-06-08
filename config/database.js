cda3c69742a7c2ef1e33ded4d4a4376c18ab0ddd
MadhuKumarPatel <madhu.puppala13@gmail.com>
2024-06-08 10:42:06 +0530

    push

--- config/database.js
+++ config/database.js
@@ -1,23 +1,17 @@
-const path = require('path');
-
-module.exports = ({ env }) => {
-  const client = env('DATABASE_CLIENT', 'sqlite');
-
-  const connections = {
-    sqlite: {
-      connection: {
-        filename: env('DATABASE_FILENAME', '/data/database.sqlite'),
-      },
-      useNullAsDefault: true,
-    },
-    // other database configurations
-  };
-
-  return {
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
     connection: {
-      client,
-      ...connections[client],
-      acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+      host: env('DATABASE_HOST', 'localhost'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'strapi'),
+      user: env('DATABASE_USERNAME', 'strapi'),
+      password: env('DATABASE_PASSWORD', 'strapi'),
+      ssl: env.bool('DATABASE_SSL', false),
     },
-  };
-};
+    pool: {
+      min: 2,
+      max: 10,
+    },
+  },
+});
